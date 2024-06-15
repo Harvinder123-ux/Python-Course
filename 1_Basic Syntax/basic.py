b3a12b0f8578fb5a6f767293916a0f55700ce788
Harvinder123-ux <leoabhiarya@gmail.com>
2024-06-15 19:36:23 +0530

    Basic operators

---
+++ 1_Basic Syntax/basic.py
@@ -0,0 +1,38 @@
+# our first python program - single line comment
+'''print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")'''
+
+
+
+print("hello abhi")
+
+
+
+#variables in python with some basic built in data types info 
+a = 9 #integer
+print(a)
+
+b = True #or False - boolean numbers
+print(b)
+
+c = "shreya" #string
+print(c)
+
+d = None #means non
+print(d)
+
+x = 17.8  #float
+y = 15.4
+
+z = x + y
+print("x + y =", z)
+
+
+
+
+if 7>3:
+    print("true 7 is greater than 3")
