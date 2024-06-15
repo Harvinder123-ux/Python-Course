ba90c977b2bc9d8f257883850fdcac8b69b4eefe
Harvinder123-ux <leoabhiarya@gmail.com>
2024-06-15 21:42:42 +0530

    Python TypeCasting

---
+++ 2_TypeCasting/typecasting.py
@@ -0,0 +1,6 @@
+#TypeCasting in Python Programming language
+a = "10"
+# print(a +1 ) #gonna show error becoz the value it is holding is string and can't concatinate with the integer 
+print(int(a) + 5) #but we can solve this proble by making a variable value to integer
+b = 10
+print(b +1 )
