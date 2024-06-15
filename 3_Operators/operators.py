cbc539b64322a0a192049dfa16092297d1f6a10f
Harvinder123-ux <leoabhiarya@gmail.com>
2024-06-15 21:43:15 +0530

    Operator In Python

---
+++ 3_Operators/operators.py
@@ -0,0 +1,41 @@
+#arithmetic operators
+a = 10
+b = 2
+
+print("a + b = ",a+b)
+print("a - b = ",a-b)
+print("a * b = ",a*b)
+print("a / b = ",a/b)
+print("a // b = ",a//b) #float divison - means remove the numbers after a point
+print("a ** b = ",a**b) #like 10  ratio 15
+print("a % b = ",a%b)   #will show the remainder
+
+
+
+#assingment operators - assigning the value (=)
+x = 7
+print(x)
+
+x += 9
+x -= 9
+x *= 9
+x /= 9
+print(x)
+
+
+#comparison operators
+x = 10
+y = 7
+z = 10
+print("X is greater then y : ",x> y)
+print("y is greater than x : ",x < 7)
+print("x is not equal to y : ",x!= y)
+print("x is euqal to z: " ,x==z)
+print("x is equal or bigger than z : ",x >= z)
+print("y is equal or bigger than y : ",x<= y)
+
+
+#logical operators
+print("And operators : ",x < y and x == z)
+print("OR operators : ",x < y or x == z)
+print("Not operators : ",not(x < y)) #reverse 
