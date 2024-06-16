27dd2f593ab2ca4eff91fe4ac4d7ac7b4c516863
Harvinder123-ux <leoabhiarya@gmail.com>
2024-06-16 14:07:00 +0530

    User Input Python

---
+++ User Input/userInput.py
@@ -0,0 +1,10 @@
+name = input("enter your name: ")
+print(name)
+
+print("For the sum of Two Values")
+num1 = input('enter your first val: ')
+
+print(" + ")
+num2 = input('enter your second val: ')
+
+print("Result: ", int(num1) + int(num2))
